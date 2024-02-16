507557899481ffca4e484ffa267d68499782a5ec
killian.carrat@etu.univ-orleans.fr <killian.carrat@etu.univ-orleans.fr>
2024-02-16 16:17:24 +0100

    Premier test

--- TestsBoxes.java
+++ TestsBoxes.java
@@ -13,4 +13,14 @@ public class TestsBoxes {
         b.add("truc1");
         b.add("truc2");
     }
+
+    @Test
+    public void testUser() {
+        User soi = new User(456, "Larue", "Lavrai");
+        Box b = new Box();
+        soi.ajoute(b);
+        b.add("machin");
+        b.add("lope");
+        
+    }
 }
